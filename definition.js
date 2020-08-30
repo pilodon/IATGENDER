c7833b994333c86d6699265574157e9b2cb78575
pilodon <69127941+pilodon@users.noreply.github.com>
2020-08-29 21:15:06 -0400

    Update definition.js

--- definition.js
+++ definition.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat8.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://pilodon.github.io/IATGENDER/mainIAT.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
